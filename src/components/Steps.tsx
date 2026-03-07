cc6338a783a1b3ed8e63c1f690a9036867c1f454
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2026-03-06 19:57:14 -0600

    6 marzo

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Steps() {
   return (
     <section className=" bg-beige-steps grid justify-center py-10">
-      <div className="grid grid-cols-2 grid-rows-3 h-screen py-20">
-        <div className="flex items-center col-start-2    border-l-3 border-light-beige relative pb-20">
+      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 md:justify-items-start grid-rows-3 h-screen py-20">
+        <div className="w-3/5 md:w-full flex items-center col-start-2 border-l-3 border-light-beige relative pb-20">
           <div className="absolute -left-10 top-0 bg-light-beige rounded-full py-6 px-8">
             <span className="font-semibold text-xl">1</span>
           </div>
@@ -15,33 +16,36 @@ export default function Steps() {
               width={80}
               height={80}
               priority
-              loading="eager"
+              className="w-14 sm:w-16 md:w-20 h-auto"
             />
-            <p className="text-3xl font-nav font-normal pt-5">Agenda tú cita</p>
+            <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-nav font-normal pt-5">
+              Agenda tú cita
+            </p>
           </div>
         </div>
-        <div className="relative flex flex-row-reverse items-center row-start-2">
-          <div className="absolute -right-10 bg-light-beige rounded-full py-6 px-8">
+        <div className="w-3/5 md:w-full relative flex items-center row-start-2 col-start-2  md:col-start-1 border-l-3 border-light-beige md:border-0">
+          {/* <div className="relative flex flex-row-reverse items-center row-start-2"> */}
+          <div className="absolute -left-10 md:left-auto md:-right-10 bg-light-beige rounded-full py-6 px-8">
             <span className="font-semibold text-xl">2</span>
           </div>
-          <div className="justify-items-center pr-20">
+          <div className="justify-items-center pl-20 md:pl-0 md:pr-20">
             <Image
               src="/images/high-five.png"
               alt="Coaching Terapéutico"
               width={80}
               height={80}
               priority
-              loading="eager"
+              className="w-14 sm:w-16 md:w-20 h-auto"
             />
-            <p className="text-3xl font-nav font-normal pt-5 w-70 text-center">
+            <p className="text-lg sm:text-xl md:text-2xl lg:3xl font-nav font-normal pt-5 lg:w-70 text-center w-7/12">
               Encuentrame en línea o en persona
             </p>
           </div>
         </div>
-        <div className="border-l-3 border-light-beige">
+        <div className="border-l-3 border-light-beige md:block hidden">
           <span className="hidden">border</span>
         </div>
-        <div className="relative flex items-center row-start-3 col-start-2 border-l-3 border-light-beige">
+        <div className="w-3/5 md:w-full relative flex items-center row-start-3 col-start-2 border-l-3 border-light-beige">
           <div className="absolute -left-10 bottom-0 bg-light-beige rounded-full py-6 px-8">
             <span className="font-semibold text-xl">3</span>
           </div>
@@ -52,18 +56,20 @@ export default function Steps() {
               width={80}
               height={80}
               priority
-              loading="eager"
+              className="w-14 sm:w-16 md:w-20 h-auto"
             />
-            <p className="text-3xl font-nav font-normal pt-5 text-text-steps">
+            <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-nav font-normal pt-5 text-text-steps">
               Empieza tú proceso
             </p>
           </div>
         </div>
       </div>
       <div className="m-auto pt-16">
-        <button className="bg-brown-cta hover:bg-brown-darker transition duration-300 focus:bg-brown-darker rounded font-semibold text-white px-10 py-5 tracking-wider">
-          Comienza hoy
-        </button>
+        <Link href="/agenda">
+          <button className="bg-brown-cta hover:bg-brown-darker transition duration-300 focus:bg-brown-darker rounded font-semibold text-white px-10 py-5 tracking-wider">
+            Comienza hoy
+          </button>
+        </Link>
       </div>
     </section>
   );
